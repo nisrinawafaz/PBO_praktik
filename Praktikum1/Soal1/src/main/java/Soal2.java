57be55a3ceb8eecedcd195abc9db345215ee83a1
nisrinawafaz <nisrinawafaz@gmail.com>
2023-09-02 14:39:41 +0700

    Soal 1.java program untuk menampilkan sebuah nilai dengan tipe data byte dan analisis mengenai range tipedata Soal 2.java program yang berisi logika decision dengan  teknik ternary

---
+++ Praktikum1/Soal1/src/main/java/Soal2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal2 {
+    public static void main(String[] args){
+        int i=42;
+        String s = (i<40)?"life":(i>50)?"universe":"everything"; 
+        // perintah decision dengan operator ternary
+        // kondisi? perintah jika true : perintah jika false
+
+        System.out.println(s); // menampilkan nilai pada variabel s
+    }
+}
---
+++ Praktikum1/Soal1/src/main/java/com/mycompany/soal1/Soal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal1 {
+    public static void main(String[] args){
+        byte angka1 =125;
+        byte angka2 =6;
+        byte hasil = (byte) (angka1+angka2); // menjumlahkan perhitungan ke dalam tipe data byte
+
+        System.out.println("Hasil "+ hasil); // memanggil var yang berisi hasil perhitungan // di java untuk menggunakan '+'
+    }
+}
