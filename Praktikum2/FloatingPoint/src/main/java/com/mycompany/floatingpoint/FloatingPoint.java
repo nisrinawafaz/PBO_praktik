d85b8404e0090115cf32a60d680b9aa77d182667
nisrinawafaz <nisrinawafaz@gmail.com>
2023-09-02 14:31:53 +0700

    Program untuk membulatkan suatu nilai dengan menggunakan fungsi math.round()

---
+++ Praktikum2/FloatingPoint/src/main/java/com/mycompany/floatingpoint/FloatingPoint.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.floatingpoint;
+
+/**
+ *
+ * @author ASUS
+ */
+public class FloatingPoint {
+
+    public static void main(String[] args) {
+        double x = 92.98; 
+        int nx = (int) Math.round(x);
+        System.out.print("nilai nx: " + nx);
+
+    }
+}
