376b7fb00ad626cb832a7c66f8530fae6a7d39b0
nisrinawafaz <nisrinawafaz@gmail.com>
2023-09-10 14:42:58 +0700

    menampilkan output nilai nx

--- Praktikum2/FloatingPoint/src/main/java/com/mycompany/floatingpoint/FloatingPoint.java
+++ Praktikum2/FloatingPoint/src/main/java/com/mycompany/floatingpoint/FloatingPoint.java
@@ -13,7 +13,5 @@ public class FloatingPoint {
     public static void main(String[] args) {
         double x = 92.98; 
         int nx = (int) Math.round(x);
-        System.out.print("nilai nx: " + nx);
-
     }
 }
