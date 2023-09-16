7a9bae1e887910ffdf3c64b6051ba0076f6567b8
nisrinawafaz <nisrinawafaz@gmail.com>
2023-09-16 17:08:12 +0700

    Urutan pemanggilan konstruksi objek dimulai dari blok inisialisasi static, blok inisialisasi non static, konstruktor.

---
+++ Praktikum4/KelasDua/src/kelasdua/KelasDua.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelasdua;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KelasDua {
+
+        {
+            System.out.println(5);
+        }
+        
+        public static void main(String[] args)
+        {
+            System.out.println(6);
+            KelasSatu satu = new KelasSatu(); 
+            System.out.println("-----");
+            KelasSatu dua = new KelasSatu(10);
+        }
+    
+}
